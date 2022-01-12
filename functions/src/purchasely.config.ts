49650c2013317c684b5c594071785062794623ea
Thomas Léger <8971906+El-Fitz@users.noreply.github.com>
2022-01-12 14:32:22 +0000

    feat(webhookV3): Implement separate collection for V3 events storage

--- functions/src/purchasely.config.ts
+++ functions/src/purchasely.config.ts
@@ -3,7 +3,7 @@ export interface PurchaselyFirestoreDestinationSettings {
   collectionName: string | null;
 }
 
-export interface PurchaselyConfigInterface {
+export interface PurchaselyConfigForWebhookVersion {
   destinations: {
     customerUsers: PurchaselyFirestoreDestinationSettings,
     firebaseCustomClaims: {
@@ -14,6 +14,11 @@ export interface PurchaselyConfigInterface {
     purchaselySubscriptions: PurchaselyFirestoreDestinationSettings,
     purchaselyEvents: PurchaselyFirestoreDestinationSettings
   },
+}
+
+export interface PurchaselyConfigInterface {
+  2: PurchaselyConfigForWebhookVersion,
+  3: PurchaselyConfigForWebhookVersion,
   functions: {
     memory: string;
     region: string;
@@ -23,30 +28,59 @@ export interface PurchaselyConfigInterface {
 }
 
 export const PurchaselyConfig: PurchaselyConfigInterface = {
-  destinations: {
-    customerUsers: {
-      enabled: false,
-      collectionName: null,
-    },
-    firebaseCustomClaims: {
-      enabled: process.env.UPDATE_FIREBASE_AUTH_CUSTOM_CLAIMS === "ENABLED",
-    },
-    purchaselyConsumables: {
-      enabled: true,
-      collectionName: process.env.PURCHASELY_CONSUMABLES_COLLECTION as string,
-    },
-    purchaselyNonConsumables: {
-      enabled: true,
-      collectionName: process.env.PURCHASELY_NON_CONSUMABLES_COLLECTION as string,
-    },
-    purchaselySubscriptions: {
-      enabled: true,
-      collectionName: process.env.PURCHASELY_SUBSCRIPTIONS_COLLECTION as string,
-    },
-    purchaselyEvents: {
-      enabled: process.env.PURCHASELY_EVENTS_COLLECTION !== undefined && process.env.PURCHASELY_EVENTS_COLLECTION !== "",
-      collectionName: process.env.PURCHASELY_EVENTS_COLLECTION ?? "",
-    },
+  2: {
+    destinations: {
+      customerUsers: {
+        enabled: false,
+        collectionName: null,
+      },
+      firebaseCustomClaims: {
+        enabled: process.env.UPDATE_FIREBASE_AUTH_CUSTOM_CLAIMS === "ENABLED",
+      },
+      purchaselyConsumables: {
+        enabled: true,
+        collectionName: process.env.PURCHASELY_CONSUMABLES_COLLECTION as string,
+      },
+      purchaselyNonConsumables: {
+        enabled: true,
+        collectionName: process.env.PURCHASELY_NON_CONSUMABLES_COLLECTION as string,
+      },
+      purchaselySubscriptions: {
+        enabled: true,
+        collectionName: process.env.PURCHASELY_SUBSCRIPTIONS_COLLECTION as string,
+      },
+      purchaselyEvents: {
+        enabled: process.env.PURCHASELY_EVENTS_COLLECTION_2 !== undefined && process.env.PURCHASELY_EVENTS_COLLECTION_2 !== "",
+        collectionName: process.env.PURCHASELY_EVENTS_COLLECTION_2 ?? "",
+      },
+    }
+  },
+  3: {
+    destinations: {
+      customerUsers: {
+        enabled: false,
+        collectionName: null,
+      },
+      firebaseCustomClaims: {
+        enabled: process.env.UPDATE_FIREBASE_AUTH_CUSTOM_CLAIMS === "ENABLED",
+      },
+      purchaselyConsumables: {
+        enabled: true,
+        collectionName: process.env.PURCHASELY_CONSUMABLES_COLLECTION as string,
+      },
+      purchaselyNonConsumables: {
+        enabled: true,
+        collectionName: process.env.PURCHASELY_NON_CONSUMABLES_COLLECTION as string,
+      },
+      purchaselySubscriptions: {
+        enabled: true,
+        collectionName: process.env.PURCHASELY_SUBSCRIPTIONS_COLLECTION as string,
+      },
+      purchaselyEvents: {
+        enabled: process.env.PURCHASELY_EVENTS_COLLECTION_3 !== undefined && process.env.PURCHASELY_EVENTS_COLLECTION_3 !== "",
+        collectionName: process.env.PURCHASELY_EVENTS_COLLECTION_3 ?? "",
+      },
+    }
   },
   functions: {
     memory: "1GB",
