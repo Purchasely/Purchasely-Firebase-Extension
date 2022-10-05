644da6a0d4932267ee8e615e5f799653287e44d3
Thomas Léger <8971906+El-Fitz@users.noreply.github.com>
2022-10-05 22:00:50 +0200

    fix(tests): fix consumables events saving tests

--- functions/__tests__/v3/Unit/Consumables/event-saving.test.ts
+++ functions/__tests__/v3/Unit/Consumables/event-saving.test.ts
@@ -1,23 +1,23 @@
 import test from "ava";
 import { DateTime } from "luxon";
 
-import { defaultSubscriptionEvent as defaultWebhookEvent } from "../../_webhookEventFactory";
+import { defaultConsumableEvent as defaultWebhookEvent } from "../../_webhookEventFactory";
 import { servicesFactory } from "../../_servicesFactory";
 
-import { saveSubscriptionEvent } from "../../../../src/v3/purchasely-webhook/subscription-webhook-event.controller"
+import { saveConsumableEvent } from "../../../../src/v3/purchasely-webhook/consumables-webhook-event.controller";
 import { PurchaselyEventDomain } from "../../../../src/v3/purchasely-events/domain/purchasely-event.domain";
 
 
 test("Event saving returns null if service is null", async t => {
   const event = defaultWebhookEvent();
   const service = null;
-  t.is(await saveSubscriptionEvent(service)(event), null);
+  t.is(await saveConsumableEvent(service)(event), null);
 });
 
 test("Event saving does not return null if service is not null", async t => {
   const event = defaultWebhookEvent();
   const service = servicesFactory().events;
-  t.not(await saveSubscriptionEvent(service)(event), null);
+  t.not(await saveConsumableEvent(service)(event), null);
 });
 
 test("Events Service Creation method is invoked", async t => {
@@ -31,7 +31,7 @@ test("Events Service Creation method is invoked", async t => {
     }
   };
   t.plan(1);
-  await saveSubscriptionEvent(service)(event);
+  await saveConsumableEvent(service)(event);
 });
 
 test("Events Service Creation method is invoked with the proper event", async t => {
@@ -52,5 +52,26 @@ test("Events Service Creation method is invoked with the proper event", async t 
     }
   };
   t.plan(8);
-  await saveSubscriptionEvent(service)(event);
+  await saveConsumableEvent(service)(event);
+});
+
+test("Events Service Creation method is successfully invoked with a consumbable event", async t => {
+  const event = defaultWebhookEvent();
+  const factoryService = servicesFactory().events;
+  const service = {
+    ...factoryService,
+    create: (id: string, eventToCreate: PurchaselyEventDomain) => {
+      t.is(typeof eventToCreate.id, "string");
+      t.is(eventToCreate.event_name, event.event_name);
+      t.is(eventToCreate.user_id, event.user_id);
+      t.is(eventToCreate.product, event.product);
+      t.is(eventToCreate.store, event.store);
+      t.is(eventToCreate.store_app_bundle_id, event.store_app_bundle_id);
+      t.is(eventToCreate.purchased_at.toISO(), DateTime.fromISO(event.purchased_at).toISO());
+      t.is(eventToCreate.event_created_at.toISO(), DateTime.fromISO(event.event_created_at).toISO());
+      return factoryService.create(id, eventToCreate);
+    }
+  };
+  t.plan(8);
+  await saveConsumableEvent(service)(event);
 });
